4e6e0112a0e85f1f1972cd880297c04a9bb0f391
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-06-08 21:57:24 +0300

    editing hexes map, added breaker descriptions

--- hexes.py
+++ hexes.py
@@ -6,6 +6,36 @@ ver_count = 50
 
 hex_width = 20
 
+land_hexes = [
+              list(map(lambda x: [0, x], range(58))) + \
+              list(map(lambda x: [1, x], range(58))) + \
+              list(map(lambda x: [2, x], range(57))) + \
+              list(map(lambda x: [3, x], range(57))) + \
+              list(map(lambda x: [4, x], range(56))) + \
+              list(map(lambda x: [5, x], range(56))) + \
+              list(map(lambda x: [6, x], range(54))) + \
+              list(map(lambda x: [7, x], range(54))) + \
+              list(map(lambda x: [8, x], range(53))) + \
+              list(map(lambda x: [9, x], range(53))) + [[9,86]] + [[9,87]] + list(map(lambda x: [9, x], range(92, 99))) + \
+              list(map(lambda x: [10, x], range(51))) + list(map(lambda x: [10, x], range(85, 89))) + list(map(lambda x: [10, x], range(91, 99))) + \
+              list(map(lambda x: [11, x], range(51))) + list(map(lambda x: [11, x], range(86, 89))) + list(map(lambda x: [11, x], range(91, 99))) + \
+              list(map(lambda x: [12, x], range(51))) + [[12, 87]] + [[12, 88]] + list(map(lambda x: [12, x], range(90, 99))) + 
+              list(map(lambda x: [13, x], range(51))) + list(map(lambda x: [13, x], range(90, 99))) + \
+              list(map(lambda x: [14, x], range(50))) + list(map(lambda x: [14, x], range(90, 99))) + \
+              list(map(lambda x: [15, x], range(50))) + list(map(lambda x: [15, x], range(91, 99))) + \
+              list(map(lambda x: [16, x], range(49))) + list(map(lambda x: [16, x], range(91, 99))) + \
+
+              list(map(lambda x: [17, x], range(51))) + list(map(lambda x: [17, x], range(90, 99))) + \
+              list(map(lambda x: [18, x], range(52))) + list(map(lambda x: [18, x], range(90, 99))) + \
+              list(map(lambda x: [19, x], range(53))) + list(map(lambda x: [19, x], range(91, 99))) + \
+              list(map(lambda x: [20, x], range(44))) + list(map(lambda x: [20, x], range(91, 99))) + \
+              list(map(lambda x: [21, x], range(56))) + list(map(lambda x: [21, x], range(91, 99))) + \
+              list(map(lambda x: [22, x], range(57))) + list(map(lambda x: [18, x], range(90, 99))) + \
+              list(map(lambda x: [23, x], range(58))) + list(map(lambda x: [19, x], range(91, 99))) + \
+              list(map(lambda x: [24, x], range(57))) + list(map(lambda x: [20, x], range(91, 99))) + \
+              list(map(lambda x: [25, x], range(57))) + list(map(lambda x: [21, x], range(91, 99)))
+             ]               
+
 def initial_state(state):
     colors = []
     for v in range(ver_count):
