b23fccd6e95e39457015a87498a56ceea1127d35
Denis Peshekhonov <denis@peshekhonov.ru>
2019-06-08 18:18:41 +0300

    datacenters

--- gamestate.py
+++ gamestate.py
@@ -1,8 +1,11 @@
 import uuid, jsonpickle, hexes, icebreakers, research, time, ship
+from datacenter import DataCenter
 
 ticks_per_second = 10
 tick_duration = round(1000 / ticks_per_second)
 start_money = 10000
+datacenter_start_cost = 100
+datacenter_cost_coeff = 1.2
 
 class GameState:
     def __init__(self):
@@ -16,9 +19,18 @@ class GameState:
         self.last_request = self.ct()
         self.money = start_money
         self.research_level = 0
+        self.datacenters = []
+        self.datacenter_cost = 0
+        self.set_next_dc_cost()
 
     def ct(self):
         return int(round(time.time() * 1000))
+
+    def set_next_dc_cost(self):
+        if self.datacenter_cost == 0:
+            self.datacenter_cost = datacenter_start_cost
+        else:
+            self.datacenter_cost = int(round(self.datacenter_cost * datacenter_cost_coeff))
 
     def update_all(self):
         t = self.ct()
@@ -33,6 +45,13 @@ class GameState:
         for rsrch in self.research:
             if (not rsrch.progress == 0 and not rsrch.progress == rsrch.maximum_progress):
                 rsrch.update(self)
+
+    def build_datacenter(self, row, col):
+        if self.money >= self.datacenter_cost:
+            self.money -= self.datacenter_cost
+            self.set_next_dc_cost()
+            self.datacenters.append(DataCenter(row, col))
+            self.research_level -= 1
     
     def get_research_by_id(self, research_id):
         return list(filter(lambda x: x.id == research_id, self.research))[0]
