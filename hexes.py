b93b6865e3636f40936335be81703f2bea489dfe
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-06-08 22:45:49 +0300

    map to file

--- hexes.py
+++ hexes.py
@@ -1,4 +1,5 @@
 import math
+import land_mask
 
 hor_count_odd = 101
 hor_count_even = hor_count_odd - 1
@@ -9,32 +10,7 @@ hex_width = 20
 normal_color = '#00ff00'
 land_color = '#ff0000'
 
-land_hexes =  list(map(lambda x: [0, x], range(58))) + list(map(lambda x: [0, x], range(94, 100))) + \
-              list(map(lambda x: [1, x], range(58))) + list(map(lambda x: [1, x], range(94, 100))) + \
-              list(map(lambda x: [2, x], range(57))) + list(map(lambda x: [2, x], range(94, 100))) + \
-              list(map(lambda x: [3, x], range(57))) + list(map(lambda x: [3, x], range(94, 100))) + \
-              list(map(lambda x: [4, x], range(56))) + list(map(lambda x: [4, x], range(93, 100))) + \
-              list(map(lambda x: [5, x], range(56))) + list(map(lambda x: [5, x], range(93, 100))) + \
-              list(map(lambda x: [6, x], range(54))) + list(map(lambda x: [6, x], range(93, 100))) + \
-              list(map(lambda x: [7, x], range(54))) + list(map(lambda x: [7, x], range(93, 100))) + \
-              list(map(lambda x: [8, x], range(53))) + list(map(lambda x: [8, x], range(92, 100))) + \
-              list(map(lambda x: [9, x], range(53))) + [[9,86]] + [[9,87]] + list(map(lambda x: [9, x], range(92, 100))) + \
-              list(map(lambda x: [10, x], range(51))) + list(map(lambda x: [10, x], range(85, 89))) + list(map(lambda x: [10, x], range(91, 100))) + \
-              list(map(lambda x: [11, x], range(51))) + list(map(lambda x: [11, x], range(86, 89))) + list(map(lambda x: [11, x], range(91, 100))) + \
-              list(map(lambda x: [12, x], range(51))) + [[12, 87]] + [[12, 88]] + list(map(lambda x: [12, x], range(90, 100))) + \
-              list(map(lambda x: [13, x], range(51))) + list(map(lambda x: [13, x], range(90, 100))) + \
-              list(map(lambda x: [14, x], range(50))) + list(map(lambda x: [14, x], range(90, 100))) + \
-              list(map(lambda x: [15, x], range(50))) + list(map(lambda x: [15, x], range(91, 100))) + \
-              list(map(lambda x: [16, x], range(49))) + list(map(lambda x: [16, x], range(91, 100))) + \
-              list(map(lambda x: [17, x], range(51))) + list(map(lambda x: [17, x], range(91, 100))) + \
-              list(map(lambda x: [18, x], range(52))) + list(map(lambda x: [18, x], range(91, 100))) + \
-              list(map(lambda x: [19, x], range(53))) + list(map(lambda x: [19, x], range(92, 100))) + \
-              list(map(lambda x: [20, x], range(47))) + list(map(lambda x: [20, x], range(48,53))) + list(map(lambda x: [20, x], range(91, 100))) + \
-              list(map(lambda x: [21, x], range(47))) + list(map(lambda x: [21, x], range(48,56))) + list(map(lambda x: [21, x], range(92, 100))) + \
-              list(map(lambda x: [22, x], range(7,46))) + list(map(lambda x: [22, x], range(48,57))) + list(map(lambda x: [22, x], range(92, 100))) + \
-              list(map(lambda x: [23, x], range(8,45))) + list(map(lambda x: [23, x], range(48,58))) + list(map(lambda x: [23, x], range(93, 100))) + \
-              list(map(lambda x: [24, x], range(13,43))) + list(map(lambda x: [24, x], range(48,57))) + list(map(lambda x: [24, x], range(93, 100))) + \
-              list(map(lambda x: [25, x], range(21,40))) +  [[25, 14]] +  [[25, 1]] + [[25, 18]] +  [[25, 19]] + list(map(lambda x: [25, x], range(50,57))) + list(map(lambda x: [25, x], range(92, 100)))             
+land_hexes = land_mask.mask 
 
 def initial_state(state):
     colors = []
@@ -86,5 +62,4 @@ def is_hex_restricted(r, c):
         if hexs[0] == r and hexs[1] == c:
             return True
     return False
-    
     
