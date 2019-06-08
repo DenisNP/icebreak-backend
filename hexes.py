c3fe1a9dda803fc6708ac2b619b55e3856cb381d
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-06-08 13:16:57 +0300

    coordinates calculations

--- hexes.py
+++ hexes.py
@@ -1,6 +1,10 @@
+import math
+
 hor_count_odd = 25
 hor_count_even = hor_count_odd - 1
 ver_count = 12
+
+hex_width = 80
 
 def initial_state(state):
     colors = []
@@ -12,3 +16,13 @@ def initial_state(state):
             colors[v].append(last_color)
 
     return colors
+
+
+def get_coords(row, column):
+    if row % 2 == 0:
+        x = int(hex_width / 2 + hex_width * column)
+    else:
+        x = int(hex_width * (column - 1))
+    y = int(hex_width / math.sqrt(3)/2 + 3/(2 * math.sqrt(3)) * hex_width * row) 
+
+    return [x, y]
