9b02be108daac6279b9d71fbfa28f5387e502483
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-06-08 11:40:09 +0300

    flask post processing

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request  #import main Flask class and request object
+import sys
+app = Flask(__name__) #create the Flask app
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World.'
-    
+@app.route('/gamestate', methods=['POST']) #GET requests will be blocked
+def gamestate_json():
+
+    req_data = request.get_json()
+
+    #print(request.args, file=sys.stderr)
+
+    user_id = request.args.get('user_id')
+    user_action = req_data['user_action']['param1']
+    last_process_timestamp = req_data['last_process_timestamp']
+
+    return '''
+           user_id is: {}
+           user_action is: {}
+           last_process_timestamp is: {}
+           '''.format(user_id, user_action, last_process_timestamp)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5000)  #run app in debug mode on port 5000
+    
+    
