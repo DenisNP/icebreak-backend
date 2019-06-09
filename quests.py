7dee8f18ac82be682ba245b2f57896fcb4097f9f
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-06-09 11:42:03 +0300

    quests added

---
+++ quests.py
@@ -0,0 +1,181 @@
+quests_delivery = [
+    {
+        'id' : 1,
+        'name' : 'Ледовая проводка в Диксон',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 2,
+        'name' : 'Проводка каравана на Чукотку',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 3,
+        'name' : 'Доставка топлива и провизии в Певек',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 4,
+        'name' : 'Проводка груза нефтепродуктов в Архангельск',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+        {
+        'id' : 5,
+        'name' : 'Научная экспедиция на северный полюс',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 6,
+        'name' : 'Туристический рейс на северный полюс',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+        {
+        'id' : 7,
+        'name' : 'Научное оборудование для дрейфующей станции',
+        'image': '',
+        'description': '',
+        'coordinates': [[1, 1]],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 8,
+        'name' : 'Груз на военно-морскую базу',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },    {
+        'id': 9,
+        'name' : 'Проводка каравана сухогрузов из Китая',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 10,
+        'name' : 'Крупный транспортный караван из Мурманска',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 11,
+        'name' : 'Проводка судов на утилизацию из Тикси',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id': 12,
+        'name' : 'Проводка каравана сухогрузов из Китая',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 13,
+        'name' : 'Крупный транспортный караван из Мурманска',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 14,
+        'name' : 'Доставка припасов на приледнившуюся подводную лодку',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 15,
+        'name' : 'Эвакуация судна замерзшего во льдах',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 16,
+        'name' : 'Доставка экологов на дрейфующую станцию',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+     {
+        'id': 17,
+        'name' : 'Прокладка кабеля для подводного датацентра',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 18,
+        'name' : 'Археологическая экспедиция по маршрутам полярных исследователей',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 19,
+        'name' : 'Помощь Норвежцам в рамках международного сотрудничества',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    },
+    {
+        'id' : 20,
+        'name' : 'Груз морепродуктов из Сабетты',
+        'image': '',
+        'description': '',
+        'coordinates': [1, 1],
+        'salary' : 1000,
+        'ttl' : 600
+    }
+]
