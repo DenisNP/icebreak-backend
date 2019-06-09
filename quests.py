ce8767702bbe0b031c53558d77e3ecac5d8552b9
Grigory Frantsuzov <gfrantsuzov@systemorph.com>
2019-06-09 12:43:37 +0300

    updated quest class

--- quests.py
+++ quests.py
@@ -1,8 +1,10 @@
+import random
+
 quests_delivery = [
     {
         'id' : 1,
         'name' : 'Ледовая проводка в Диксон',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -11,7 +13,7 @@ quests_delivery = [
     {
         'id' : 2,
         'name' : 'Проводка каравана на Чукотку',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -20,7 +22,7 @@ quests_delivery = [
     {
         'id' : 3,
         'name' : 'Доставка топлива и провизии в Певек',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -29,7 +31,7 @@ quests_delivery = [
     {
         'id' : 4,
         'name' : 'Проводка груза нефтепродуктов в Архангельск',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -38,7 +40,7 @@ quests_delivery = [
         {
         'id' : 5,
         'name' : 'Научная экспедиция на северный полюс',
-        'image': '',
+        'image': 'quests/science.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -47,7 +49,7 @@ quests_delivery = [
     {
         'id' : 6,
         'name' : 'Туристический рейс на северный полюс',
-        'image': '',
+        'image': 'quests/tent.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -55,8 +57,8 @@ quests_delivery = [
     },
         {
         'id' : 7,
-        'name' : 'Научное оборудование для дрейфующей станции',
-        'image': '',
+        'name' : 'Оборудование для дрейфующей станции',
+        'image': 'quests/science.png',
         'description': '',
         'coordinates': [[1, 1]],
         'salary' : 1000,
@@ -65,7 +67,7 @@ quests_delivery = [
     {
         'id' : 8,
         'name' : 'Груз на военно-морскую базу',
-        'image': '',
+        'image': 'quests/submarine.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -73,7 +75,7 @@ quests_delivery = [
     },    {
         'id': 9,
         'name' : 'Проводка каравана сухогрузов из Китая',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -82,7 +84,7 @@ quests_delivery = [
     {
         'id' : 10,
         'name' : 'Крупный транспортный караван из Мурманска',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -91,7 +93,7 @@ quests_delivery = [
     {
         'id' : 11,
         'name' : 'Проводка судов на утилизацию из Тикси',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -99,8 +101,8 @@ quests_delivery = [
     },
     {
         'id': 12,
-        'name' : 'Проводка каравана сухогрузов из Китая',
-        'image': '',
+        'name' : 'Экологическая научная экспедиция',
+        'image': 'quests/bear.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -108,8 +110,8 @@ quests_delivery = [
     },
     {
         'id' : 13,
-        'name' : 'Крупный транспортный караван из Мурманска',
-        'image': '',
+        'name' : 'Исследования полярной фауны',
+        'image': 'quests/bear.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -118,7 +120,7 @@ quests_delivery = [
     {
         'id' : 14,
         'name' : 'Доставка припасов на приледнившуюся подводную лодку',
-        'image': '',
+        'image': 'quests/submarine.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -127,7 +129,7 @@ quests_delivery = [
     {
         'id' : 15,
         'name' : 'Эвакуация судна замерзшего во льдах',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -136,7 +138,7 @@ quests_delivery = [
     {
         'id' : 16,
         'name' : 'Доставка экологов на дрейфующую станцию',
-        'image': '',
+        'image': 'quests/science.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -145,7 +147,7 @@ quests_delivery = [
      {
         'id': 17,
         'name' : 'Прокладка кабеля для подводного датацентра',
-        'image': '',
+        'image': 'quests/science.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -154,7 +156,7 @@ quests_delivery = [
     {
         'id' : 18,
         'name' : 'Археологическая экспедиция по маршрутам полярных исследователей',
-        'image': '',
+        'image': 'quests/tent.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -163,7 +165,7 @@ quests_delivery = [
     {
         'id' : 19,
         'name' : 'Помощь Норвежцам в рамках международного сотрудничества',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
@@ -172,10 +174,24 @@ quests_delivery = [
     {
         'id' : 20,
         'name' : 'Груз морепродуктов из Сабетты',
-        'image': '',
+        'image': 'quests/ship.png',
         'description': '',
         'coordinates': [1, 1],
         'salary' : 1000,
         'ttl' : 600
     }
 ]
+
+class Quest:
+    quests =[]
+
+    def __init__(self, ship_count):
+        self.completed = False
+
+        if (ship_count > 1):
+            for shp in range(ship_count):
+                self.quests.append(self.get_quest())
+
+    def get_quest(self):
+        return quests_delivery[random.randint(1,20)]
+    
