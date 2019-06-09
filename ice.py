e8c8a71e624ffc92ffc9890f12e8fbf764d3ec0a
Denis Peshekhonov <denis@peshekhonov.ru>
2019-06-09 12:53:28 +0300

    ice only trail

--- ice.py
+++ ice.py
@@ -41,66 +41,67 @@ class Ice:
         self.ver_speed += random.randint(-1, 1) * basic_speed
 
     def generate_next_field(self):
-        try:
-            self.start_field = copy.deepcopy(self.current_field)
-        except:
-            pass
+        pass
+        # try:
+        #     self.start_field = copy.deepcopy(self.current_field)
+        # except:
+        #     pass
 
-        world = np.zeros(shape)
-        for i in range(shape[0]):
-            for j in range(shape[1]):
-                world[i][j] = round(pnoise2((i + self.hor_shift)/scale, (j + self.ver_shift)/scale, octaves=octaves, lacunarity=lacunarity, base=seed)*resolution)/resolution
+        # world = np.zeros(shape)
+        # for i in range(shape[0]):
+        #     for j in range(shape[1]):
+        #         world[i][j] = round(pnoise2((i + self.hor_shift)/scale, (j + self.ver_shift)/scale, octaves=octaves, lacunarity=lacunarity, base=seed)*resolution)/resolution
 
-        ice = []
-        for r in range(len(world)):
-            ice.append([])
-            for c in range(len(world[r])):
-                if hexes.is_hex_exists(r, c):
-                    val = (world[r][c] - min_val) / (max_val - min_val)
-                    if val > 1:
-                        val = 1
-                    if val < 0:
-                        val = 0
-                    ice[r].append(int(round(val * 100)))
-                else:
-                    ice[r].append(0)
+        # ice = []
+        # for r in range(len(world)):
+        #     ice.append([])
+        #     for c in range(len(world[r])):
+        #         if hexes.is_hex_exists(r, c):
+        #             val = (world[r][c] - min_val) / (max_val - min_val)
+        #             if val > 1:
+        #                 val = 1
+        #             if val < 0:
+        #                 val = 0
+        #             ice[r].append(int(round(val * 100)))
+        #         else:
+        #             ice[r].append(0)
         
-        self.next_field = ice
+        # self.next_field = ice
 
     def update(self):
-        pass
-        # if self.current_phase >= ticks_between_states:
-        #     self.move_field()
-        #     self.generate_next_field()
-        #     self.current_phase = 0
+        #pass
+        if self.current_phase >= ticks_between_states:
+            self.move_field()
+            self.generate_next_field()
+            self.current_phase = 0
         
-        # for i in range(len(self.current_field)):
-        #     for k in range(len(self.current_field[i])):
-        #         next_val = self.next_field[i][k]
-        #         start_val = self.start_field[i][k]
-        #         self.current_field[i][k] = int(round(start_val + (next_val - start_val) * (self.current_phase / ticks_between_states)))
-        #         trail = str(i * 1000 - (-k))
-        #         if trail in self.trails:
-        #             #print("Set trail " + trail, self.trails[trail]/10)
-        #             self.current_field[i][k] = max(0, int(round(self.current_field[i][k] - self.trails[trail]/10)))
+        for i in range(len(self.current_field)):
+            for k in range(len(self.current_field[i])):
+                next_val = self.next_field[i][k]
+                start_val = self.start_field[i][k]
+                self.current_field[i][k] = int(round(start_val + (next_val - start_val) * (self.current_phase / ticks_between_states)))
+                trail = str(i * 1000 - (-k))
+                if trail in self.trails:
+                    #print("Set trail " + trail, self.trails[trail]/10)
+                    self.current_field[i][k] = max(0, int(round(self.current_field[i][k] - self.trails[trail]/10)))
 
-        # keys = list(self.trails.keys())
-        # for trail in keys:
-        #     self.trails[trail] -= 1
-        #     if self.trails[trail] <= 0:
-        #         self.trails.pop(trail, None)
+        keys = list(self.trails.keys())
+        for trail in keys:
+            self.trails[trail] -= 1
+            if self.trails[trail] <= 0:
+                self.trails.pop(trail, None)
 
-        # self.current_phase += 1
+        self.current_phase += 1
 
     def place_ship(self, hex):
-        pass
-        # i = hex[0]
-        # k = hex[1]
-        # trail = str(i * 1000 - (-k))
-        # if trail in self.trails:
-        #     self.trails[trail] += 500
-        # else:
-        #     self.trails[trail] = 500
+        #pass
+        i = hex[0]
+        k = hex[1]
+        trail = str(i * 1000 - (-k))
+        if trail in self.trails:
+            self.trails[trail] += 500
+        else:
+            self.trails[trail] = 500
 
 if __name__ == '__main__':
     world = np.zeros(shape)
